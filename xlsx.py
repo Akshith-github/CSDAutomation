38dc2e3dd5cf33a52dd2cd3e569b63bb02f4bdc7
Akshith <akshithnukala9@gmail.com>
2022-05-15 16:54:31 +0000

    commentations

--- xlsx.py
+++ xlsx.py
@@ -1,5 +1,16 @@
 import pandas as pd
 import os
+'''
+This script is used to create a Base XLSX file intended to be used as a template for the 
+CSD automation
+
+It contains the following columns:
+    1. id_no
+    2. status
+    3.clientMail
+    4.Message
+'''
+
 
 os.chdir("C:/Users/akshi/Box")
 df1 = pd.DataFrame(columns=["id_no","status","clientMail","Message"],)
